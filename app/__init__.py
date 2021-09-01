1b518e87dde0b5b22565545e0fdcfcb448a7b602
Ethan Shealey <ethan.shealey@gmail.com>
2021-09-01 00:11:27 -0400

    auto commit

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ import pymysql
 app = Flask(__name__)
 login = LoginManager(app)
 login.login_view = 'login'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://b2cdbfe6672f14:01921dc8@us-cdbr-east-04.cleardb.com/heroku_39c807b6bb86bdc?reconnect=true"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://b2cdbfe6672f14:01921dc8@us-cdbr-east-04.cleardb.com/heroku_39c807b6bb86bdc"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.config.from_object(Config)
