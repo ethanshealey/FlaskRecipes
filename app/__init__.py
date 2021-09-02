e37c32d7893ddb8905d221c565d3e5023e5315b5
Ethan Shealey <ethan.shealey@gmail.com>
2021-09-01 21:28:09 -0400

    auto commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import pymysql
+import pymysql, os
 
 app = Flask(__name__)
+app.config.from_object(Config)
 login = LoginManager(app)
 login.login_view = 'login'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:pass@127.0.0.1/recipes"
-app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.config.from_object(Config)
 
 from app import routes
